c15b6c98e1aebd5c3bb59c63e6a5398354e0c45d
deSpooky <phurrde@gmail.com>
2025-04-29 19:39:26 +0300

    second ct with migrations!!

---
+++ !!!.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from models import Artist, Album, Song, db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route('/songs')
+def songs():
+    songs_list = Song.query.all()
+    return render_template('songs.html', songs=songs_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
-from models import Artist, Album, Song, db
+import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-db.init_app(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.route('/songs')
-def songs():
-    songs_list = Song.query.all()
-    return render_template('songs.html', songs=songs_list)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50))  
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 if __name__ == '__main__':
     app.run(debug=True)
